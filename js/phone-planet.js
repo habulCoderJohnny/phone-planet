070f15a690d5fda81acff13f38999c5a22032bd7
habulCoderJohnny <fokruljohnny@gmail.com>
2022-03-02 17:21:21 +0600

    get phone-details & created cols

--- js/phone-planet.js
+++ js/phone-planet.js
@@ -16,7 +16,8 @@ const searchPhone = () => {
     const SearchResult = document.getElementById('search-result');
     SearchResult.textContent = ''; //← ← clear previous search result
     if (phones.length == '') {
-      document.getElementById('message').style.display = 'block';}
+      document.getElementById('message').style.display = 'block';
+    }
       else {
         document.getElementById('message').style.display = 'none';
         if (phones.length >= 20 || phones.length < 20) {
@@ -37,9 +38,49 @@ const searchPhone = () => {
         </div>
       </div> `;
       SearchResult.appendChild(div);
-      spinner.style.display = "none";
     })
   }
 }
 
-}              
+} 
+
+// get phone-Id | get phone details    
+const loadPhoneById = phoneId => {
+  // console.log(phoneId);
+  const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`;
+  fetch(url)
+  .then(response => response.json())
+  .then(json => displayPhoneById(json.data))
+}
+
+// phone details part |get id| create div, add card and phone details
+const displayPhoneById = phone => {
+  // console.log(phone);
+  const phoneDetails = document.getElementById('phone-details');
+  phoneDetails.textContent = ''; //← ← clear previous phone details
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `
+  <div class="card h-100 w-100 p-2 ">
+  <img src="${phone.image}" class="card-img-top img-fluid w-25" alt="...">
+  
+  <div class="card-body d-flex flex-column align-items-center container-sm">
+    <p class="card-text fw-bolder text-primary">Brand:${phone.brand}</p>
+    <h5 class="card-title text-primary">Phone Name:${phone.name}</h5>
+    <u class="card-text fw-bolder text-primary">Release Date:${phone.releaseDate ? phone.releaseDate : 'Release Date Not Available!'}</u>
+    <p class="card-text fw-bolder text-primary">Storage:${phone.mainFeatures.storage}</p>
+    <p class="card-text fw-bolder text-primary">Memory:${phone.mainFeatures.memory}</p>
+    <p class="card-text fw-bolder text-primary">Display:${phone.mainFeatures.displaySize}</p>
+    <p class="card-text fw-bolder text-primary">Chipset:${phone.mainFeatures.chipSet}</p>
+    <p class="card-text text-break fw-bolder text-primary">Sensor:${phone.mainFeatures.sensors}
+    <p class="card-text text-break fw-bolder text-primary">WLAN:${phone.others.WLAN}
+    <p class="card-text text-break fw-bolder text-primary">Bluetooth:${phone.others.Bluetooth}
+    <p class="card-text text-break fw-bolder text-primary">GPS:${phone.others.GPS}
+    <p class="card-text text-break fw-bolder text-primary">NFC: ${phone.others.NFC} Radio: ${phone.others.Radio}
+    <p class="card-text text-break fw-bolder text-primary">${phone.others.USB}
+    
+  </div>   
+</div>`;
+phoneDetails.appendChild(div);
+}
+
